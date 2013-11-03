a319fda1f5387e5751a45d9e635a1c5e7f51b3e7
Jeroen De Dauw <jeroendedauw@gmail.com>
2013-11-03 01:03:58 +0100

    Update ExtensionInstaller.php

--- ExtensionInstaller.php
+++ ExtensionInstaller.php
@@ -3,3 +3,13 @@
 if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
 	include_once( __DIR__ . '/vendor/autoload.php' );
 }
+
+$wgExtensionCredits['other']['ExtensionInstaller'] = array(
+	'path' => __FILE__,
+	'name' => 'Extension Installer',
+	'version' => '0.1',
+	'author' => array(
+		'[https://www.mediawiki.org/wiki/User:Jeroen_De_Dauw Jeroen De Dauw]',
+	),
+	'url' => 'https://github.com/JeroenDeDauw/ExtensionInstaller'
+);
