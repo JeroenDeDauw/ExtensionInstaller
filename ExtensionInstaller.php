163305111cfcecb133e9fc786848c5606d640ba5
Jeroen De Dauw <jeroendedauw@gmail.com>
2013-12-02 14:42:16 +0100

    Update ExtensionInstaller.php

--- ExtensionInstaller.php
+++ ExtensionInstaller.php
@@ -1,6 +1,6 @@
 <?php
 
-if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
+if ( is_readable( __DIR__ . '/../../vendor/autoload.php' ) ) {
 	include_once( __DIR__ . '/../../vendor/autoload.php' );
 }
 
