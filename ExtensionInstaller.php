9254d7b2f348d72d5cb4499dba5c84dd26e6be4b
Jeroen De Dauw <jeroendedauw@gmail.com>
2013-12-02 14:37:52 +0100

    Update ExtensionInstaller.php

--- ExtensionInstaller.php
+++ ExtensionInstaller.php
@@ -1,7 +1,7 @@
 <?php
 
 if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
-	include_once( __DIR__ . '/vendor/autoload.php' );
+	include_once( __DIR__ . '/../../vendor/autoload.php' );
 }
 
 $GLOBALS['wgExtensionCredits']['other']['ExtensionInstaller'] = array(
